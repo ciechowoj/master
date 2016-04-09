6298bd4c322c8184a5a79e6039ed1397e1fd0424
ciechowoj <keepitsimplesirius@gmail.com>
2016-04-09 23:47:38 +0200

    KDTree update 2.

--- KDTree3D.hpp
+++ KDTree3D.hpp
@@ -2,6 +2,7 @@
 #include <glm>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 namespace haste {
 
@@ -38,6 +39,12 @@ public:
         return (data[item] >> shift) & mask;
     }
 
+    void swap(size_t a, size_t b) {
+        size_t t = get(a);
+        set(a, get(b));
+        set(b, t);
+    }
+
 private:
     vector<size_t> data;
 };
@@ -60,31 +67,32 @@ public:
     }
 
     KDTree3D(vector<T>&& that) {
-        data = move(that);
-        flags.resize(data.size());
+        _data = move(that);
+        flags.resize(_data.size());
 
-        if (!data.empty()) {
-            vec3 lower = position(data[0]), upper = position(data[0]);
+        if (!_data.empty()) {
+            vec3 lower = position(_data[0]), upper = position(_data[0]);
 
-            for (size_t i = 0; i < data.size(); ++i) {
-                vec3 pos = position(data[i]);
+            for (size_t i = 0; i < _data.size(); ++i) {
+                vec3 pos = position(_data[i]);
                 lower = min(lower, pos);
                 upper = max(upper, pos);
             }
 
-            vector<size_t> X(data.size());
-            vector<size_t> Y(data.size());
-            vector<size_t> Z(data.size());
-            vector<size_t> unique(data.size());
+            vector<size_t> X(_data.size());
+            vector<size_t> Y(_data.size());
+            vector<size_t> Z(_data.size());
+            vector<size_t> unique(_data.size());
+            vector<size_t> scratch(_data.size());
 
             iota(X);
             iota(Y);
             iota(Z);
             iota(unique);
 
-            sort<0>(X, unique, data);
-            sort<1>(Y, unique, data);
-            sort<2>(Z, unique, data);
+            sort<0>(X, unique, _data);
+            sort<1>(Y, unique, _data);
+            sort<2>(Z, unique, _data);
 
             size_t* ranges[3] = {
                 X.data(),
@@ -92,7 +100,26 @@ public:
                 Z.data()
             };
 
-            build(0, data.size(), make_pair(lower, upper), ranges, unique.data());
+            build(
+                0, 
+                _data.size(), 
+                make_pair(lower, upper), 
+                ranges, 
+                unique.data(),
+                scratch.data());
+
+            for (size_t i = 0; i < _data.size(); ++i) {
+                size_t j = i;
+                size_t k = X[j];
+
+                while (k != X[k]) {
+                    swap(_data[j], _data[X[j]]);
+                    // flags.swap(j, X[j]);
+                    X[j] = j;
+                    j = k;
+                    k = X[k];
+                }
+            }
         }
     }
 
@@ -101,8 +128,49 @@ public:
 
     vector<T> query_k(size_t k) const;
 
+    const T* data() const {
+        return _data.data();
+    }
+
+    size_t size() const {
+        return _data.size();
+    }
+
+    size_t axis() const {
+        return flags.get(size() / 2);
+    }
+
+    KDTree3D copy_left() const {
+        KDTree3D result;
+        size_t median = size() / 2;
+        result._data.resize(median);
+        result.flags.resize(median);
+
+        for (size_t i = 0; i < median; ++i) {
+            result._data[i] = _data[i];
+            result.flags.set(i, flags.get(i));
+        }
+
+        return result;
+    }
+
+    KDTree3D copy_right() const {
+        KDTree3D result;
+        size_t median = size() / 2;
+        size_t rsize = size() - median - 1;
+        result._data.resize(rsize);
+        result.flags.resize(rsize);
+
+        for (size_t i = 0; i < rsize; ++i) {
+            result._data[i] = _data[i + median + 1];
+            result.flags.set(i, flags.get(i + median + 1));
+        }
+
+        return result;
+    }
+
 private:
-    vector<T> data;
+    vector<T> _data;
     BitfieldVector<2> flags;
 
     static const size_t leaf = 3;
@@ -128,48 +196,75 @@ private:
         size_t end,
         size_t median,
         size_t* subranges[3],
-        size_t* unique) 
+        size_t* unique,
+        size_t* scratch) 
     {
-        size_t size = end - begin;
-        size_t adjoint = subranges[axis][median];
+        size_t median_index = subranges[axis][median];
 
-        if (adjoint != median) {
-            for (size_t j = 0; j < 3; ++j) {
-                for (size_t i = begin; i < end; ++i) {
-                    if (subranges[j][i] == median) {
-                        subranges[j][i] = adjoint;
-                        break;
-                    }
+        for (size_t j = 0; j < 3; ++j) {
+            if (axis != j) {
+                size_t* subrange = subranges[j];
+                size_t itr = begin;
+
+                while (subrange[itr] != median_index) {
+                    ++itr;
                 }
 
-                subranges[j][median] = median;
-            }
+                while (itr < median) {
+                    swap(subrange[itr], subrange[itr + 1]);
+                    ++itr;
+                }
 
-            swap(data[median], data[adjoint]);
-            swap(unique[median], unique[adjoint]);
+                while (median < itr) {
+                    swap(subrange[itr - 1], subrange[itr]);
+                    --itr;
+                }
+            }
         }
 
         auto less = [&](size_t a, size_t b) -> bool {
-            return data[a][axis] == data[b][axis] 
+            return _data[a][axis] == _data[b][axis] 
                 ? unique[a] < unique[b] 
-                : data[a][axis] < data[b][axis];
+                : _data[a][axis] < _data[b][axis];
         };
 
         for (size_t j = 0; j < 3; ++j) {
             size_t* subrange = subranges[j];
-            size_t l = begin;
-            size_t g = median + 1;
 
-            while (g < end) {
-                if (less(subrange[g], median)) {
-                    while (less(subrange[l], median)) {
-                        ++l;
-                    }
+            for (size_t i = begin; i < end; ++i) {
+                scratch[i] = subrange[i];
+            }
 
-                    swap(subrange[l], subrange[g]);
+            size_t lst_dst = begin;
+            size_t geq_dst = median + 1;
+
+            size_t lst_src = begin;
+            size_t geq_src = median + 1;
+
+            while (lst_src < median) {
+                if (less(scratch[lst_src], median_index)) {
+                    subrange[lst_dst] = scratch[lst_src];
+                    ++lst_dst;
+                }
+                else {
+                    subrange[geq_dst] = scratch[lst_src];
+                    ++geq_dst;
                 }
 
-                ++g;
+                ++lst_src;
+            }
+
+            while (geq_src < end) {
+                if (less(scratch[geq_src], median_index)) {
+                    subrange[lst_dst] = scratch[geq_src];
+                    ++lst_dst;
+                }
+                else {
+                    subrange[geq_dst] = scratch[geq_src];
+                    ++geq_dst;
+                }
+
+                ++geq_src;
             }
         }
     }
@@ -179,22 +274,24 @@ private:
         size_t end,
         const pair<vec3, vec3>& aabb,
         size_t* subranges[3],
-        size_t* unique)
+        size_t* unique,
+        size_t* scratch)
     {
         size_t size = end - begin;
 
         if (size > 1) {
             size_t axis = max_axis(aabb);
             size_t median = begin + size / 2;
-            rearrange(axis, begin, end, median, subranges, unique);
+
+            rearrange(axis, begin, end, median, subranges, unique, scratch);
             flags.set(median, axis);
 
             pair<vec3, vec3> left_aabb = aabb, right_aabb = aabb;
-            left_aabb.second[axis] = data[median][axis];
-            right_aabb.first[axis] = data[median][axis];
+            left_aabb.second[axis] = _data[median][axis];
+            right_aabb.first[axis] = _data[median][axis];
 
-            build(begin, median, left_aabb, subranges, unique);
-            build(median + 1, end, left_aabb, subranges, unique);
+            build(begin, median, left_aabb, subranges, unique, scratch);
+            build(median + 1, end, right_aabb, subranges, unique, scratch);
         }
         else if (size == 1) {
             flags.set(begin, leaf);
--- unit_tests/KDTree3D.test.cpp
+++ unit_tests/KDTree3D.test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <KDTree3D.hpp>
+#include <iostream>
 
 using namespace glm;
 using namespace haste;
@@ -14,6 +15,11 @@ TEST(BitfieldVector, basic_tests) {
     EXPECT_EQ(2, b.get(0));
     EXPECT_EQ(1, b.get(1));
     EXPECT_EQ(0, b.get(2));
+
+    b.swap(0, 1);
+
+    EXPECT_EQ(1, b.get(0));
+    EXPECT_EQ(2, b.get(1));
 
     BitfieldVector<3> c(100);
     c.set(99, 5);
@@ -35,21 +41,103 @@ TEST(KDTree3D, should_create_empty) {
 
 TEST(KDTree3D, should_create_singleton) {
     KDTree3D<vec3> a({ vec3(0.0f) });
+    EXPECT_EQ(3, a.axis());
 }
 
 TEST(KDTree3D, should_create_from_bunch_of_points) {
     vector<vec3> points = {
+        vec3(5.0f, 3.5f, -1.0f),
         vec3(2.0f, 1.0f, 1.0f),
         vec3(3.5f, 5.0f, 0.0f),
         vec3(1.5f, 4.0f, 2.0f),
-        vec3(5.0f, 3.5f, -1.0f),
         vec3(2.5f, 2.5f, 0.0f),
         vec3(5.0f, 2.0f, 0.0f),
         vec3(2.5f, 3.5f, 0.0f),
     };
 
     KDTree3D<vec3> a(points);
+
+    EXPECT_EQ(1, a.axis());
+
+    auto bottom = a.copy_left();
+    auto top = a.copy_right();
+
+    EXPECT_EQ(3, bottom.size());
+    EXPECT_EQ(0, bottom.axis());
+    EXPECT_EQ(3, top.size());
+    EXPECT_EQ(0, top.axis());
+
+    auto left = top.copy_left();
+    auto right = top.copy_right();
+
+    EXPECT_EQ(1, left.size());
+    EXPECT_EQ(3, left.axis());
+    EXPECT_EQ(1, right.size());
+    EXPECT_EQ(3, right.axis());
 }
 
+TEST(KDTree3D, slightly_unbalanced) {
+    vector<vec3> points = {
+        vec3(5.0f, 3.5f, -1.0f),
+        vec3(2.0f, 1.0f, 1.0f),
+    };
 
+    KDTree3D<vec3> a(points);
 
+    EXPECT_EQ(2, a.size());
+    EXPECT_EQ(0, a.axis());
+
+    EXPECT_EQ(1, a.copy_left().size());
+    EXPECT_EQ(0, a.copy_right().size());
+}
+
+TEST(KDTree3D, test_y_split) {
+    vector<vec3> points = {
+        vec3(5.0f, 3.5f, -1.0f),
+        vec3(2.0f, -3.5f, 1.0f),
+    };
+
+    KDTree3D<vec3> a(points);
+
+    EXPECT_EQ(1, a.axis());
+}
+
+TEST(KDTree3D, test_z_split) {
+    vector<vec3> points = {
+        vec3(5.0f, 3.5f, -10.0f),
+        vec3(2.0f, -3.5f, 1.0f),
+    };
+
+    KDTree3D<vec3> a(points);
+
+    EXPECT_EQ(2, a.axis());
+}
+
+TEST(KDTree3D, test_string_alike) {
+    vector<vec3> points = {
+        vec3(0, 1, 0),
+        vec3(0, 2, 0),
+        vec3(0, 3, 0),
+        vec3(0, 4, 0),
+        vec3(0, 5, 0),
+        vec3(0, 6, 0),
+        vec3(0, 7, 0),
+        vec3(0, 8, 0),
+    };
+
+    KDTree3D<vec3> a(points);
+
+    EXPECT_EQ(1, a.axis());
+
+    auto b = a.copy_left();
+    auto c = a.copy_right();
+
+    EXPECT_EQ(1, b.axis());
+    EXPECT_EQ(4, b.size());
+    EXPECT_EQ(1, c.axis());
+    EXPECT_EQ(3, c.size());
+
+    EXPECT_TRUE(vec3(0, 6, 0) == c.data()[0]);
+    EXPECT_TRUE(vec3(0, 7, 0) == c.data()[1]);
+    EXPECT_TRUE(vec3(0, 8, 0) == c.data()[2]);
+}
