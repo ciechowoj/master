77627de1e721cd075062a4f01e60d83fea3a1c75
ciechowoj <keepitsimplesirius@gmail.com>
2016-12-07 18:11:05 +0100

    Format PT.

--- PT.cpp
+++ PT.cpp
@@ -1,118 +1,111 @@
-#include <PT.hpp>
 #include <Edge.hpp>
+#include <PT.hpp>
 
 namespace haste {
 
-PathTracing::PathTracing(size_t minSubpath, float roulette, float beta, size_t num_threads)
-    : Technique(num_threads), _minSubpath(minSubpath), _roulette(roulette), _beta(beta) { }
+PathTracing::PathTracing(size_t minSubpath, float roulette, float beta,
+                         size_t num_threads)
+    : Technique(num_threads),
+      _minSubpath(minSubpath),
+      _roulette(roulette),
+      _beta(beta) {}
 
 vec3 PathTracing::_traceEye(render_context_t& context, Ray ray) {
-    vec3 radiance = vec3(0.0f);
-    EyeVertex eye[2];
-    size_t itr = 0, prv = 1;
+  vec3 radiance = vec3(0.0f);
+  EyeVertex eye[2];
+  size_t itr = 0, prv = 1;
 
-    RayIsect isect = _scene->intersect(ray.origin, ray.direction);
+  RayIsect isect = _scene->intersect(ray.origin, ray.direction);
 
-    while (isect.isLight()) {
-        radiance += _scene->queryRadiance(isect, -ray.direction);
-        isect = _scene->intersect(isect.position(), ray.direction);
-    }
+  while (isect.isLight()) {
+    radiance += _scene->queryRadiance(isect, -ray.direction);
+    isect = _scene->intersect(isect.position(), ray.direction);
+  }
 
-    if (!isect.isPresent()) {
-        return radiance;
-    }
+  if (!isect.isPresent()) {
+    return radiance;
+  }
 
-    eye[prv].surface = _scene->querySurface(isect);
-    eye[prv].omega = -ray.direction;
-    eye[prv].throughput = vec3(1.0f);
-    eye[prv].specular = 0.0f;
-    eye[prv].density = 1.0f;
+  eye[prv].surface = _scene->querySurface(isect);
+  eye[prv].omega = -ray.direction;
+  eye[prv].throughput = vec3(1.0f);
+  eye[prv].specular = 0.0f;
+  eye[prv].density = 1.0f;
 
-    size_t path_size = 2;
+  size_t path_size = 2;
+
+  while (true) {
+    radiance += _connect(context, eye[prv]);
+
+    auto bsdf =
+        _scene->sampleBSDF(*context.engine, eye[prv].surface, eye[prv].omega);
 
     while (true) {
-        radiance += _connect(context, eye[prv]);
+      isect = _scene->intersect(isect.position(), bsdf.omega);
 
-        auto bsdf = _scene->sampleBSDF(*context.engine, eye[prv].surface, eye[prv].omega);
+      if (!isect.isPresent()) {
+        return radiance;
+      }
 
-        while (true) {
-            isect = _scene->intersect(isect.position(), bsdf.omega);
+      eye[itr].surface = _scene->querySurface(isect);
+      eye[itr].omega = -bsdf.omega;
 
-            if (!isect.isPresent()) {
-                return radiance;
-            }
+      auto edge = Edge(eye[prv], eye[itr]);
 
-            eye[itr].surface = _scene->querySurface(isect);
-            eye[itr].omega = -bsdf.omega;
+      eye[itr].throughput =
+          eye[prv].throughput * bsdf.throughput * edge.bCosTheta / bsdf.density;
 
-            auto edge = Edge(eye[prv], eye[itr]);
+      eye[prv].specular = bsdf.specular;
+      eye[itr].density = eye[prv].density * edge.fGeometry * bsdf.density;
 
-            eye[itr].throughput
-                = eye[prv].throughput
-                * bsdf.throughput
-                * edge.bCosTheta
-                / bsdf.density;
+      if (isect.isLight()) {
+        auto lsdf = _scene->queryLSDF(eye[itr].surface, eye[itr].omega);
+        float weightInv =
+            lsdf.areaDensity() / (edge.fGeometry * bsdf.density) + 1.0f;
 
-            eye[prv].specular = bsdf.specular;
-            eye[itr].density = eye[prv].density * edge.fGeometry * bsdf.density;
+        if (bsdf.specular == 1.0f) weightInv = 1.0f;
 
-            if (isect.isLight()) {
-                auto lsdf = _scene->queryLSDF(eye[itr].surface, eye[itr].omega);
-                float weightInv = lsdf.areaDensity() / (edge.fGeometry * bsdf.density) + 1.0f;
-
-                if (bsdf.specular == 1.0f)
-                    weightInv = 1.0f;
-
-                radiance += lsdf.radiance() * eye[itr].throughput / weightInv;
-            }
-            else {
-                break;
-            }
-        }
-
-        std::swap(itr, prv);
-
-        float roulette = path_size < _minSubpath ? 1.0f : _roulette;
-        float uniform = sampleUniform1(*context.engine).value();
-
-        if (roulette < uniform) {
-            return radiance;
-        }
-        else {
-            eye[prv].throughput /= roulette;
-            ++path_size;
-        }
+        radiance += lsdf.radiance() * eye[itr].throughput / weightInv;
+      } else {
+        break;
+      }
     }
 
-    return radiance;
+    std::swap(itr, prv);
+
+    float roulette = path_size < _minSubpath ? 1.0f : _roulette;
+    float uniform = sampleUniform1(*context.engine).value();
+
+    if (roulette < uniform) {
+      return radiance;
+    } else {
+      eye[prv].throughput /= roulette;
+      ++path_size;
+    }
+  }
+
+  return radiance;
 }
 
 vec3 PathTracing::_connect(render_context_t& context, const EyeVertex& eye) {
-    LightSample light = _scene->sampleLight(*context.engine);
-    vec3 omega = normalize(eye.surface.position() - light.position());
+  LightSample light = _scene->sampleLight(*context.engine);
+  vec3 omega = normalize(eye.surface.position() - light.position());
 
-    if (dot(omega, light.normal()) < 0.0f) {
-        return vec3(0.0f);
-    }
+  if (dot(omega, light.normal()) < 0.0f) {
+    return vec3(0.0f);
+  }
 
-    auto eyeBSDF = _scene->queryBSDF(eye.surface, -omega, eye.omega);
+  auto eyeBSDF = _scene->queryBSDF(eye.surface, -omega, eye.omega);
 
-    auto edge = Edge(light, eye, omega);
+  auto edge = Edge(light, eye, omega);
 
-    float weightInv = eyeBSDF.densityRev * edge.bGeometry / light.areaDensity() + 1.0f;
+  float weightInv =
+      eyeBSDF.densityRev * edge.bGeometry / light.areaDensity() + 1.0f;
 
-    return _scene->occluded(eye.surface.position(), light.position())
-        * light.radiance()
-        / light.areaDensity()
-        * eye.throughput
-        * eyeBSDF.throughput
-        * edge.bCosTheta
-        * edge.fGeometry
-        / weightInv;
+  return _scene->occluded(eye.surface.position(), light.position()) *
+         light.radiance() / light.areaDensity() * eye.throughput *
+         eyeBSDF.throughput * edge.bCosTheta * edge.fGeometry / weightInv;
 }
 
-string PathTracing::name() const {
-    return "Path Tracing";
-}
-
+string PathTracing::name() const { return "Path Tracing"; }
 }
--- PT.hpp
+++ PT.hpp
@@ -4,27 +4,27 @@
 namespace haste {
 
 class PathTracing : public Technique {
-public:
-    PathTracing(size_t minSubpath, float roulette, float beta, size_t num_threads);
+ public:
+  PathTracing(size_t minSubpath, float roulette, float beta,
+              size_t num_threads);
 
-    vec3 _traceEye(render_context_t& context, Ray ray) override;
+  vec3 _traceEye(render_context_t& context, Ray ray) override;
 
-    string name() const override;
+  string name() const override;
 
-private:
-	struct EyeVertex {
-        SurfacePoint surface;
-        vec3 omega;
-        vec3 throughput;
-        float specular;
-        float density;
-    };
+ private:
+  struct EyeVertex {
+    SurfacePoint surface;
+    vec3 omega;
+    vec3 throughput;
+    float specular;
+    float density;
+  };
 
-    vec3 _connect(render_context_t& context, const EyeVertex& eye);
+  vec3 _connect(render_context_t& context, const EyeVertex& eye);
 
-    const size_t _minSubpath;
-    const float _roulette;
-    const float _beta;
+  const size_t _minSubpath;
+  const float _roulette;
+  const float _beta;
 };
-
 }
