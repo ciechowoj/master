9d93e013bd6a61a3a42d193ab73017b56a685860
ciechowoj <keepitsimplesirius@gmail.com>
2017-09-03 20:53:54 +0200

    Update unit tests.

--- unit_test.ps1
+++ unit_test.ps1
@@ -1,14 +1,32 @@
 
+param (
+    [switch]$justPrint = $false
+)
+
 $testCases = Get-ChildItem "models" `
-    | Where-Object { $_.Name -match "TestCase1.+\.blend$" } `
+    | Where-Object { $_.Name -match "TestCase.*\.blend$" } `
     | ForEach-Object {  @{ Input = (Resolve-Path $_.FullName -Relative); BaseOutput = ("test_results\" + $_.BaseName) } }
 
-$masterPath = "master.exe"
+$masterPath = "./master.exe"
 
-mkdir test_results -Force
+function Invoke-Master() {
+    if ($justPrint) {
+        Write-Host master @args
+    }
+    else {
+        if ([System.IO.File]::Exists($masterPath)) {
+            . $masterPath @args
+        }
+        else {
+            . master @args
+        }
+    }
+}
+
+mkdir test_results -Force | Out-Null
 
 foreach ($testCase in $testCases) {
-    $baseArguments = @("--parallel", "--batch", "--beta=2", "--num-minutes=15")
-    . $masterPath $baseArguments --BPT              $testCase.Input ("--output=" + $testCase.BaseOutput + ".BPT2.exr")
-    . $masterPath $baseArguments --UPG --radius=0.1 $testCase.Input ("--output=" + $testCase.BaseOutput + ".UPG2.exr")
+    $baseArguments = @("--parallel", "--batch", "--beta=2", "--num-minutes=20")
+    Invoke-Master $baseArguments --BPT $testCase.Input ("--output=" + $testCase.BaseOutput + ".BPT2.exr")
+    Invoke-Master $baseArguments --UPG $testCase.Input ("--output=" + $testCase.BaseOutput + ".UPG2.exr")
 }
